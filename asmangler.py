734ed44d21569c7e5588ec3852d7c3ee4c34588f
sibwara <42293505+sibwara@users.noreply.github.com>
2020-06-08 17:15:38 +0200

    Update asmangler.py

--- asmangler.py
+++ asmangler.py
@@ -22,11 +22,13 @@ common_words = [
         "sys",
         "admin",
         "pass",
-        "123",
-        "1234"
+        "Pass",
+        "Admin",
+        "Adm",
+        ""
         ]
 
-suffix = [
+common_suffix = [
         "!",
         "@",
         "$",
@@ -39,43 +41,33 @@ suffix = [
         ""
         ]
 
-def leet_variation(word, res):
+common_numeric = [str(i) for i in range(2010,int(time.strftime("%Y"))+1)] + [str(i)[-2:] for i in range(2000,int(time.strftime("%Y"))+1)] + ["2k"+str(i)[-2:] for i in range(2010,int(time.strftime("%Y"))+1)] + ["2K"+str(i)[-2:] for i in range(2010,int(time.strftime("%Y"))+1)] + [str(i) for i in range(10)] + ['123', '1234'] + ['']
+
+def leet_substitution(words):
     """take two depth of l33t substitution"""
-    for key, values in leet_swap.items():
-        for value in values:
-            for key2, values2 in leet_swap.items():
-                for value2 in values2:
-                    first = re.sub(key, value, word, flags=re.I)
-                    sec = re.sub(key2, value2, first, flags=re.I)
-                    for j in suffix:
-                        res.add(first + j)
-                        res.add(sec + j)
+    res = set()
+    for word in words:
+        for key, values in leet_swap.items():
+            for value in values:
+                for key2, values2 in leet_swap.items():
+                    for value2 in values2:
+                        first = re.sub(key, value, word, flags=re.I)
+                        sec = re.sub(key2, value2, first, flags=re.I)
+                        res.add(first)
+                        res.add(sec)
     return res
 
-def common_variation(word, res):
-    for i in common_words:
-        for suff in suffix:
-            res.add(i + word + suff)
-            res.add(word + i + suff)
+def common_variation(words):
+    res = set()
+    for word in words:
+        for i in common_words:
+            for j in common_numeric:
+                for k in common_suffix:
+                    res.add(i + word + j + k)
+                    res.add(word + i + j + k)
     return res
 
-def years_variation(word, res):
-    for i in range(2010, int(time.strftime("%Y"))+1):
-        for j in suffix:
-            res.add(word + str(i) + j)
-            res.add(word + str(i).replace('0','k') + j)
-            res.add(word + str(i).replace('0','K') + j)
-            res.add(word + str(i-2000) + j)
-    return res
-
-def number_variation(word, res):
-    for i in range(10):
-        for j in suffix:
-            res.add(word+str(i)+j)
-            res.add(word+"0"+str(i)+j)
-    return res
-
-def combine(words):
+def combine(words, perm):
     res = set()
     # add each word, its capitalized version and its short name
     for word in words:
@@ -85,47 +77,36 @@ def combine(words):
         l = len(word)
         mid = word[0:l//2+l%2]
         res.add(mid)
+        res.add(mid.capitalize())
+        res.add(mid.upper())
     # add each 2 words permutations
-    for p in itertools.product(res, repeat=2):
-        res.add(''.join(p))
-    # add mid word and duplicate mid word
-    print(*(res), sep='\n')
+    if perm:
+        for p in itertools.product(res, repeat=2):
+            res.add(''.join(p))
     return res
 
-def variate(words):
-    res = set()
-    for word in words:
-        # reverse
-        res.add(word[::-1])
-        # common prefix and suffix
-        res = common_variation(word, res)
-        # years
-        res = years_variation(word, res)
-        # numbers
-        res = number_variation(word, res)
-        # l33t
-        res = leet_variation(word, res)
-    print(*(res), sep='\n')
-
-def mangle(words_file):
+def mangle(words_file, perm):
     """get the base words then apply variations"""
     # combine the base words
-    words = combine(words_file.read().lower().splitlines())
-    variate(words)
+    words = combine(words_file.read().lower().splitlines(), perm)
+    words = common_variation(words)
+    words = leet_substitution(words)
+    print(*(words), sep='\n')
 
 def main():    
     parser = argparse.ArgumentParser(description='Derivation of a wordlist to make a efficient crack dictionnary', add_help=True)
     parser.add_argument('-f', action="store", dest="input_file", default=None, 
             help='Specify a wordlist file, if not, stdin is read')
+    parser.add_argument('--no-perm', action="store_false", dest="perm", default=True,
+            help='Do not mix words 2 by 2, only perform variations over unique words')
     args = parser.parse_args()
 
     if args.input_file:
         with open(args.input_file, "r") as words_file:
-            mangle(words_file)
+            mangle(words_file, args.perm)
     else:
         words_file = sys.stdin
-        mangle(words_file)
+        mangle(words_file, args.perm)
 
 if __name__ == '__main__':
     main()
-
