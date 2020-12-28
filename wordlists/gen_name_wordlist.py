d184f358f42e9340e0ec1499cac60b1b2a8e5bd3
sibwara <42293505+sibwara@users.noreply.github.com>
2020-12-28 01:10:35 +0100

    Add files via upload

--- wordlists/gen_name_wordlist.py
+++ wordlists/gen_name_wordlist.py
@@ -2,23 +2,76 @@
 
 import time
 import requests
+import logging as log
+import click
 
+#########
+# CONST #
+#########
+
+export_filename = 'names.wordlist'
+
+########
+# INIT #
+########
+
+# init logging format
+log.basicConfig(format='%(asctime)s %(message)s', datefmt='%H:%M:%S', level=log.INFO)
+
+# useful for voyel substitution: david -> dvd, julia -> jl, roxane -> rxn
 root = str.maketrans('','','aeiouy')
 
+# get the list of common firstnames from various languages
+log.info("[*] Requesting firstnames list from github...")
 r = requests.get("https://raw.githubusercontent.com/danielmiessler/SecLists/master/Usernames/Names/names.txt")
+if not r.ok:
+    log.error("[!] download failed")
+    exit(1)
+log.info("[+] list downloaded")
+
+# the list of firstnames
 names = r.text.split("\n")
+
+# init the wordlist
 words = set()
-for year in range(int(time.strftime("%Y"))-100, int(time.strftime("%Y"))+1):
-    y = str(year)
-    suffix = ['', y, y + '!', y[2:], y[2:] + '!']
-    for name in names:
+
+birthdates = []
+for m in [str(i).zfill(2) for i in range(1,13)]:
+    for d in [str(i).zfill(2) for i in range(1,32)]:
+        birthdates += [ d+m, d+m+'!', m+d, m+d+'!' ]
+
+for y in map(str, range(1913, int(time.strftime("%Y"))+1)):
+    birthdates += ['', y, y + '!', y[2:], y[2:] + '!']
+
+birthdates = set(birthdates)
+
+########
+# COOK #
+########
+
+# for each year since 100 years ago to now
+log.info("[*] Computing combination for each name...")
+
+with click.progressbar(names) as namesbar:
+     for name in names:
         name = name.lower()
+        # prefix for eg Nicolas: nicolas, nic, nico, nini, ncls
         prefix = [name, name[:3], name[:4], name[:2] + name[:2], name.translate(root)]
         for p in prefix:
-            for s in suffix:
+            for s in birthdates:
+                # derivate each candidate with various case
                 words.add( p+s )
                 words.add( p.upper()+s )
                 words.add( p.capitalize()+s )
 
-with open('names.wordlist', 'w') as f:
+log.info("[+] Complete")
+
+##########
+# EXPORT #
+##########
+
+# write the wordlist into a file
+log.info(f"[*] Exporting to the file {export_filename}...")
+with open(export_filename, 'w') as f:
     print(*(words), sep='\n', file=f)
+log.info("[+] Export complete, all is finished")
