23486d9bf26b81cf83fcb688de1df67ab1b90286
sibwara <42293505+sibwara@users.noreply.github.com>
2020-12-28 01:52:17 +0100

    Add files via upload

--- wordlists/gen_date_wordlist.py
+++ wordlists/gen_date_wordlist.py
@@ -1,23 +1,69 @@
 #!/usr/bin/python3
 
 import time
+import logging as log
+import click
+
+#########
+# CONST #
+#########
+
+export_filename = 'dates.wordlist'
 
 months_en = [ "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december", "spring", "summer", "autumn", "winter" ]
 months_nl = [ "januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december", "lente", "zomer", "herfst", "winter" ]
 months_fr = [ "janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre", "printemps", "ete", "automne", "hivers" ]
 
+########
+# INIT #
+########
+
+# init logging format
+log.basicConfig(format='%(asctime)s %(message)s', datefmt='%H:%M:%S', level=log.INFO)
+
+# merge all the known months names
 months = [ months_en, months_fr, months_nl ]
 
+# init the wordlist
 words = set()
-for year in range(1950, int(time.strftime("%Y"))+1):
-    y = str(year)
-    suffix = ['', y, y + '!', y[2:], y[2:] + '!']
-    for l in months:
+
+suffix = []
+for y in map(str, range(1950, int(time.strftime("%Y"))+1)):
+    # for 1988 add 1988, 1988!, 88, 88!
+    suffix += ['', y, y + '!', y[2:], y[2:] + '!']
+    if int(y) > 2009:
+        # for 2017, add 2k17, 2K17, 2k17!, 2K17!
+        suffix += ["2k" + y[2:], "2K" + y[2:], "2k" + y[2:] + '!', "2K" + y[2:] + '!']
+
+suffix = set(suffix)
+
+########
+# COOK #
+########
+
+# for each year since 100 years ago to now
+log.info("[*] Computing combination for each language...")
+
+with click.progressbar(months) as monthsbar:
+    for l in monthsbar:
         for m in l:
-            prefix = ['', m, m.upper(), m.capitalize(), m[:3], m[:3].upper(), m[:3].capitalize()]
+            # for december add december, dec
+            prefix = ['', m.lower(), m.lower()[:3]]
             for p in prefix:
                 for s in suffix:
-                    words.add( p+s )        
+                # derivate each candidate with various case
+                    words.add( p+s )
+                    words.add( p.upper()+s )
+                    words.add( p.capitalize()+s )
 
-with open('dates.wordlist', 'w') as f:
+log.info("[+] Complete")
+
+##########
+# EXPORT #
+##########
+
+# write the wordlist into a file
+log.info(f"[*] Exporting to the file {export_filename}...")
+with open(export_filename, 'w') as f:
     print(*(words), sep='\n', file=f)
+log.info("[+] Export complete, all is finished")
