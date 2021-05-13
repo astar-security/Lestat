032656668c4ca32baee5fc8e52d9ca5c53374054
sibwara <42293505+sibwara@users.noreply.github.com>
2021-05-13 21:15:51 +0200

    Update JohnTheRipper.py

--- JohnTheRipper.py
+++ JohnTheRipper.py
@@ -5,12 +5,18 @@ import click
 import logging as log
 import requests
 import time
+import re
+import itertools
 
-log.basicConfig(format='%(asctime)s %(message)s', datefmt='%H:%M:%S', level=log.INFO)
+log.basicConfig(format=' %(asctime)s %(message)s', datefmt='%H:%M:%S', level=log.INFO)
 
 ########
 # COOK #
 ########
+
+### dict
+
+dictpath = "/usr/share/dict/words"
 
 ### birthdates
 
@@ -55,9 +61,92 @@ for y in map(str, range(1913, int(time.strftime("%Y"))+1)):
 
 years = set(years)
 
+### l33t swap
+
+leet_swap = {
+        's':['5','$'],
+        'e':['3'],
+        'a':['4','@'],
+        'o':['0'],
+        'i':['1','!'],
+        'g':['9'],
+        't':['7']
+        }
+
+### suffixes
+
+common_special = [
+        "!",
+        "@",
+        "$",
+        "%",
+        "&",
+        "*",
+        "?",
+        ".",
+        ""
+        ]
+
+common_words = [
+        "pw",
+        "pwd",
+        "admin",
+        "pass",
+        "adm",
+        ""
+        ]
+
+common_numeric = set([''])
+# 2010 -> now
+common_numeric.update( [str(i) for i in range(2010, int(time.strftime("%Y"))+1)] )
+# 2k10 -> now
+common_numeric.update( ["2k"+str(i)[-2:] for i in range(2010, int(time.strftime("%Y"))+1)] )
+# 2K10 -> now
+common_numeric.update( ["2K"+str(i)[-2:] for i in range(2010, int(time.strftime("%Y"))+1)] )
+# 0 -> 9
+common_numeric.update( [str(i) for i in range(10)] )
+# 00 -> 99
+common_numeric.update( [str(i).zfill(2) for i in range(100)] )
+# common numbers
+common_numeric.update( ['123', '1234'] )
+
+
+def leet_variation(words):
+    global leet_swap
+    res = set()
+    first_pass = []
+    for word in words:
+        res.add(word)
+        if word.isalpha() and len(set(word)) > 1 and len(word) > 2:
+            needles = [c for c in leet_swap.keys() if c in word.lower()]
+            for i in range(len(needles)):
+                nee1 = needles[i]
+                for sub in leet_swap[nee1]:
+                    first_pass.append(re.sub(nee1, sub, word, flags=re.I))
+                res |= set(first_pass)
+                for j in range(i+1,len(needles)):
+                    nee2 = needles[j]
+                    for word2 in first_pass:
+                        for sub in leet_swap[nee2]:
+                            res.add(re.sub(nee2, sub, word2, flags=re.I))
+                first_pass = []
+    return res
+
+
+def case_variation(words):
+    res = set()
+    for word in words:
+        res.update( [word, word.upper(), word.capitalize()] )
+    return res
+
+common_prefix = case_variation(leet_variation(common_words))
+common_suffix = set([n + s for n in common_numeric for s in common_special])
+common_complete = set(itertools.product(common_prefix, common_suffix))
+
 #########
 # UTILS #
 #########
+
 
 def downloadWordlist(wordlist):
     log.info("[*] Requesting wordlist...")
@@ -90,13 +179,14 @@ def passwd2NTLM(passwd):
 
 def johnIt(ntlm, candidates, reason):
     cpt = 0
-    for c in candidates:
-        hc = passwd2NTLM(c)
-        if hc in ntlm["safe"]:
-            ntlm["cracked"][hc] = {"password": c, "reason": reason, "accounts": list(ntlm["safe"][hc])}
-            del(ntlm["safe"][hc])
-            log.info(f"[+] Password found: '{c}' for {ntlm['cracked'][hc]['accounts']}")
-            cpt += 1
+    with click.progressbar(candidates) as candidatesbar:
+        for c in candidatesbar:
+            hc = passwd2NTLM(c)
+            if hc in ntlm["safe"]:
+                ntlm["cracked"][hc] = {"password": c, "reason": reason, "accounts": list(ntlm["safe"][hc])}
+                del(ntlm["safe"][hc])
+                log.info(f"[+] Password found: '{c}' for {ntlm['cracked'][hc]['accounts']}")
+                cpt += 1
     return ntlm, cpt
 
 def johnItWithWordlist(ntlm, wordlist, reason):
@@ -119,7 +209,7 @@ def johnItWithWordlist(ntlm, wordlist, reason):
 
 def strat_top10(ntlm):
     log.info("[*] Testing top 10 most common passwords...")
-    candidates = ('', '1234', '123456', '12345678', 'password', 'Password', 'PASSWORD', 'qwerty', '123123', 'ABC123')
+    candidates = ('', '1234', '123456', '12345678', 'password', 'Password', 'Passw0rd', 'test', '123123', 'abc123')
     ntlm, cpt = johnIt(ntlm, candidates, "top10")
     log.info(f"[*] {cpt} new passwords cracked, {len(ntlm['safe'])} remaining")
     return ntlm
@@ -142,7 +232,6 @@ def strat_top1M(ntlm):
 
 def strat_names(ntlm):
     log.info("[*] Testing names and birthdates...")
-    tot =0
     wordlist = "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Usernames/Names/names.txt"
     names = downloadWordlist(wordlist)
     candidates = set()
@@ -153,14 +242,15 @@ def strat_names(ntlm):
         for p in prefix:
             for s in birthdates:
                 candidates.update(allCase(p+s))
-        ntlm, cpt = johnIt(ntlm, candidates, "name")
-        tot += cpt
-    log.info(f"[*] {tot} new passwords cracked, {len(ntlm['safe'])} remaining")
+    ntlm, cpt = johnIt(ntlm, candidates, "name")
+    log.info(f"[*] {cpt} new passwords cracked, {len(ntlm['safe'])} remaining")
     del(candidates)
     del(names)
     return ntlm
 
 def strat_dates(ntlm):
+    global months
+    global years
     log.info("[*] Testing dates...")
     candidates = set()
     for month in months:
@@ -174,6 +264,17 @@ def strat_dates(ntlm):
     del(candidates)
     return ntlm
 
+def strat_words(ntlm):
+    global dictpath
+    log.info("[*] Testing words...")
+    candidates = set()
+    with open(dictpath) as f:
+        for line in f:
+            candidates.update( allCase(line[:-1]) )
+    ntlm, cpt = johnIt(ntlm, candidates, "words")
+    log.info(f"[*] {cpt} new passwords cracked, {len(ntlm['safe'])} remaining")
+    del(candidates)
+    return ntlm
 
 
 def crack(ntlm):
@@ -186,7 +287,9 @@ def crack(ntlm):
     # 4th strategy    
     ntlm = strat_dates(ntlm)
     # 5th strategy    
-    ntlm = strat_names(ntlm)
+    #ntlm = strat_names(ntlm)
+    # 6th strategy
+    ntlm = strat_words(ntlm)
     return ntlm
 
 ##########
@@ -217,4 +320,3 @@ def main(hash_file):
 
 if __name__ == '__main__':
     main()
-
